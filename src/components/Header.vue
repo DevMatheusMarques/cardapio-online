87a2c6d67b2b4854d3ed4299310111011ff93973
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-07-24 04:06:24 -0300

    style: adjustment responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,10 +10,10 @@ export default {
       <img src="/public/assets/hamb-1.png"
            alt="Logotipo"
            class="w-32 h-32 rounded-full shadow-lg hover:scale-110 duration-200">
-      <h1 class="text-2xl mt-4 mb-2 font-bold text-white px-2">Império Hamburgueria</h1>
-      <span class="text-white font-medium text-xs">Rua das Flores, 89, Centro - Presidente Prudente/SP</span>
+      <h1 class="text-2xl mt-4 mb-2 font-bold text-white px-2 sm:text-4xl">Império Hamburgueria</h1>
+      <span class="text-white font-medium text-xs sm:text-base">Rua das Flores, 89, Centro - Presidente Prudente/SP</span>
       <div class="bg-green-600 px-4 py-1 rounded-lg mt-5" id="date-span">
-        <span class="text-white font-medium">Segunda a Domingo - 18:00 as 00:00</span>
+        <span class="text-white font-medium sm:text-lg">Segunda a Domingo - 18:00 as 00:00</span>
       </div>
     </div>
   </header>
