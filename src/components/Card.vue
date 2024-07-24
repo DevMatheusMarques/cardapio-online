481d17103a53ac6502d4c863ea0c934ef46eb012
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-07-24 02:04:25 -0300

    feat: Created menu component.

---
+++ src/components/Card.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: "CardBurger",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+    dataName: {
+      type: String,
+      required: true,
+    },
+    dataDescription: {
+      type: String,
+      required: true,
+    },
+    dataPrice: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    formattedPrice() {
+      return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(this.dataPrice);
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="flex gap-2 box-border shadow-md p-4 rounded-2xl">
+    <img
+        :src="src"
+        :alt="alt"
+        class="w-28 h-28 rounded-md mt-2.5 hover:scale-110 hover:-rotate-2 duration-300">
+
+    <div class="w-full">
+      <p class="font-bold">{{ dataName }}</p>
+      <p class="text-sm">{{ dataDescription }}</p>
+
+      <div class="flex items-center gap-2 justify-between mt-3">
+        <p class="font-bold text-lg">{{ formattedPrice }}</p>
+        <button class="bg-gray-900 px-5 py-2 rounded text-white hover:bg-gray-700">
+          <font-awesome-icon
+              icon="fa fa-cart-plus"
+              class="text-lg"/>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Adicione estilos adicionais aqui, se necessário */
+</style>
--- src/views/DashboardMenu.vue
+++ src/views/DashboardMenu.vue
@@ -1,14 +1,17 @@
 <script>
 import Header from "@/components/Header.vue";
+import Menu from "@/components/Menu.vue";
 
 export default {
   name: "DashboardMenu",
-  components: {Header}
+  components: {Menu, Header}
 }
 </script>
 
 <template>
   <Header/>
+  <h1 class="text-2xl md:text-3xl font-bold text-center mt-9 mb-6">Conheça Nosso Cardápio</h1>
+  <Menu/>
 </template>
 
 <style scoped>
