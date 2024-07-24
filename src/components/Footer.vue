91d8d3a717676a5f194935019e0037a43f0f4f0e
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-07-24 02:26:30 -0300

    feat: Created footer component.

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<template>
+  <button class="flex items-center gap-2 text-white font-bold" id="cart-btn">
+    (<span class="cart-count">2</span>)
+    Ver Meu Carrinho
+    <font-awesome-icon
+        icon="fa fa-cart-plus"
+        class="text-lg text-white"/>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/DashboardMenu.vue
+++ src/views/DashboardMenu.vue
@@ -1,10 +1,11 @@
 <script>
 import Header from "@/components/Header.vue";
 import Menu from "@/components/Menu.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "DashboardMenu",
-  components: {Menu, Header}
+  components: {Footer, Menu, Header}
 }
 </script>
 
@@ -12,6 +13,7 @@ export default {
   <Header/>
   <h1 class="text-2xl md:text-3xl font-bold text-center mt-9 mb-6">Conheça Nosso Cardápio</h1>
   <Menu/>
+  <Footer class="w-full bg-red-500 py-3 fixed bottom-0 z-40 flex items-center justify-center"/>
 </template>
 
 <style scoped>
